5473f83a044a02de49f92f53f1e894920d05c9e8
roydonrebello <98824271+roydonrebello@users.noreply.github.com>
2025-01-31 12:23:34 +0530

    Add files via upload

---
+++ MediaPlayer.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adapterpattern;
+
+/**
+ *
+ * @author roydo
+ */
+public interface MediaPlayer {
+    void play(String audioType,String fileName);
+}
